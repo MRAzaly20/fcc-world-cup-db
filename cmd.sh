6af428503f16c402d86c61101243b2b0c057e113
MRAzaly20 <muhammadrazalyy@gmail.com>
2024-04-28 11:05:05 +0700

    initial

---
+++ cmd.sh
@@ -0,0 +1,7 @@
+echo "# fcc-world-cup-db" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/MRAzaly20/fcc-world-cup-db.git
+git push -u origin main
